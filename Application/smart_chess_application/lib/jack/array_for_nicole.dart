8bfd1972d3fa30d556ba58fbe174a72e87fc646c
Jack Marshall <112056604+Jacamarsh@users.noreply.github.com>
2024-12-05 16:25:49 -0700

    little led_handler.pyupdate

--- Application/smart_chess_application/lib/jack/array_for_nicole.dart
+++ Application/smart_chess_application/lib/jack/array_for_nicole.dart
@@ -3,6 +3,7 @@ import 'package:smart_chess_application/models/chesspiece.dart';
 
 class ArrayForNicole {
   static late List<List<int>> _storedArray;
+  static int playerTurnIncrementer = 0;
 
   /// Handles the 2D array for Game Mode 1.
   static List<List<int>> handleArray(List<List<int>> array) {
@@ -13,7 +14,7 @@ class ArrayForNicole {
       [0, 0, 0, 0, 0, 0, 0, 0, array[0][8], array[0][9]], // Row 0
       [0, 7, 0, 0, 0, 0, 0, 0, array[1][8], array[1][9]], // Row 1
       [0, 7, 0, 0, 0, 0, 0, 0, array[2][8], array[2][9]], // Row 2
-      [0, 0, 0, 0, 0, 0, 0, 0, array[3][8], array[3][9]], // Row 3
+      [0, 0, 0, 0, 0, 0, 0, 0, normalTurnScenario(), array[3][9]], // Row 3
       [0, 0, 0, 0, 0, 0, 0, 0, array[4][8], array[4][9]], // Row 4
       [0, 0, 0, 0, 0, 0, 0, 0, array[5][8], array[5][9]], // Row 5
       [0, 0, 0, 0, 0, 0, 0, 0, array[6][8], array[6][9]], // Row 6
@@ -38,6 +39,14 @@ class ArrayForNicole {
     Server.chessKey.currentState?.updateInt(); // Update chessboard view here
 
     return _storedArray; // Return updated array
+  }
+
+  // Every time the 2d array being sent back, change from player 1 to 2 for testing
+  // Starts with player 1
+  static int normalTurnScenario() {
+    playerTurnIncrementer += 1; // Increment the value by 1
+    return playerTurnIncrementer % 2 == 0 ? 2 : 1; // Return player 2 is up if even, player 1 if odd
+
   }
 
   static ChessPieceTeam intToChessTeam(int i) {
